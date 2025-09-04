b6b451d22f409a126ce53a9721c5c5bef66837fa
armaan535kh <kh.armaan535@gmail.com>
2025-09-04 11:19:50 +0530

    [Created] Defined the core properties and implemented the required protocol

---
+++ ToDoList/ToDoListView.swift
@@ -0,0 +1,18 @@
+//
+//  ToDoListView.swift
+//  ToDoList
+//
+//  Created by Armaan Khan  on 04/09/25.
+//
+
+import SwiftUI
+
+struct ToDoListView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ToDoListView()
+}
