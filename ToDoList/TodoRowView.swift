53ae47cadcd1e062c06bbbf5c040655dd7f02c1c
armaan535kh <kh.armaan535@gmail.com>
2025-09-06 09:21:55 +0530

    [Created] Created TodoRowView for the app

---
+++ ToDoList/TodoRowView.swift
@@ -0,0 +1,18 @@
+//
+//  TodoRowView.swift
+//  ToDoList
+//
+//  Created by Armaan Khan  on 05/09/25.
+//
+
+import SwiftUI
+
+struct TodoRowView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    TodoRowView()
+}
